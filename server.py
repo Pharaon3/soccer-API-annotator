d794bd7f9dc92a87c1a01b59f9d8419ebd5c07f1
Pharaon3 <Pharaon313@outlook.com>
2026-05-19 04:21:51 +0900

    port updated

--- server.py
+++ server.py
@@ -691,4 +691,4 @@ async def serve_styles() -> FileResponse:
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run("server:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("server:app", host="0.0.0.0", port=8080, reload=True)
